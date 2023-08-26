b146536c5b1f216e8c30675b38ed20d2c015887b
Никита Межецкий <nsm020598@gmail.com>
2023-08-26 16:34:28 +1000

    Настройка Хэширования + устранения ошибок

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,36 @@
 const path = require('path');
-const CopyPlagins = require('copy-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/main.js',
   output: {
-    filename: 'bundle.js',
+    filename: 'bundle.[contenthash]js',
     path: path.resolve(__dirname, 'build'),
     clean: true,
   },
   devtool: 'source-map',
-  plugins : [
-    new CopyPlagins(
-      {
-        patterns: [{from: 'public'}]
-      }
-    )
+  plugins: [
+    new HtmlPlugin({
+      template: 'public/index.html',
+    }),
+    new CopyPlugin({
+      patterns: [{
+        from: 'public',
+        globOptions: {
+          ignore: ['**/index.html'],
+        }
+      }]
+    })
   ],
   module: {
     rules: [
       {
-        test:/\.js/,
-        exlude: /(node_modules)/,
+        test: /\.js/,
+        exclude: /(node_modules)/,
         use: {
-          loaders: 'babel - loaders',
-          options: {presets: ['@babel/preset-env']}
+          loader: 'babel-loader',
+          options: { presets: ['@babel/preset-env'] }
         }
       }
     ]
