e62f1df8b0fe58d01a14e50739345a6e32158e7b
Никита Межецкий <nsm020598@gmail.com>
2023-08-29 21:47:27 +1000

    Исправление ошибки

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -1,7 +1,6 @@
 import { render } from '../render.js';
 import ListSorting from '../view/main/list-sorting-view.js';
 import ListPoint from '../view/main/list-point-view.js';
-import ItemListPoint from '../view/main/item-list-point-view.js';
 import FormNewPoint from '../view/main/new-form-point-view.js';
 import FormEditingPoint from '../view/main/form-editing-point-view.js';
 import Point from '../view/main/point-view.js';
@@ -10,7 +9,6 @@ export default class MainPresenter {
   listSotring = new ListSorting;
   listPoint = new ListPoint;
   newFormPoint = new FormNewPoint;
-  itemListPoint = new ItemListPoint;
   formEditingPoint = new FormEditingPoint;
 
   constructor (container) {
