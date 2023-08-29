e4d094927e78e7ed24ae4929e61b1188a7e93135
Никита Межецкий <nsm020598@gmail.com>
2023-08-29 21:44:35 +1000

    Выводим все в точки входа

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import HeaderPresenter from './presenter/header-presenter.js';
+import MainPresenter from './presenter/main-presenter.js';
+
+
+const tripMain = document.querySelector('.trip-main');
+const tripMainFilters = tripMain.querySelector('.trip-controls__filters');
+const mainContainer = document.querySelector('.trip-events');
+
+const headerPresenter = new HeaderPresenter(tripMain, tripMainFilters);
+
+const mainPresenter = new MainPresenter (mainContainer);
+
+headerPresenter.init();
+mainPresenter.init();
