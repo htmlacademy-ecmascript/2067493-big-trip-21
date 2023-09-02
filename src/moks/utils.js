ad7eeee85c234106fd36a8512f0d18173cded6f0
Никита Межецкий <nsm020598@gmail.com>
2023-09-02 23:13:25 +1000

    Создавния функций для генерации

---
+++ src/moks/utils.js
@@ -0,0 +1,16 @@
+// Функция которая дает рандомное число из задаваемого отрезка
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(a, b));
+  const upper = Math.floor(Math.max(a, b));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+// функция для поиска случайного элемента в массиве
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+//Создание рандомного ID
+const getRandomId = (length = 5) => Math.random().toString(36).substring(2, length + 2);
+
+export {getRandomInteger, getRandomArrayElement, getRandomId};
+
