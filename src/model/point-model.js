0e79e729c1f48ad5bb31fb140e387c12b298b5d5
Никита Межецкий <nsm020598@gmail.com>
2023-09-05 14:38:47 +1000

    Уберет вывод в консоль

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -6,12 +6,10 @@ export default class PointsModel {
   points = Array.from({length: POINT_COUNT}, createRoutePoint);
   pointsEditing = createRoutePoint();
   getPoints () {
-    console.log(this.points);
     return this.points;
   }
 
   getPointEdithing () {
-    console.log(this.pointsEditing);
     return this.pointsEditing;
   }
 }
